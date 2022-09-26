2664ec48b9e5e6d1e6a5aefd49af3553dc841308
shakespeare <619371428@qq.com>
2022-09-26 23:40:52 +0800

    Add:  初始化项目

--- src/main.js
+++ src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// 引入路由
+import router from '@/router'
+
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
+  // 配置路由  router: router
+  router
 }).$mount('#app')
